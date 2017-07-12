5141d0039a5034ec812b999419737be9af6cc57b
Brose Johnstone <baby-bell@users.noreply.github.com>
2017-07-12 13:27:08 -0700

    Add additional sensor

--- hibike/riab_tests/basic.py
+++ hibike/riab_tests/basic.py
@@ -1,12 +1,14 @@
 import unittest
 from ..hibike_tester import Hibike
 from ..hibike_process import identify_smart_sensors, get_working_serial_ports
+from ..hibike_message import all_params_for_device_id
 from ..run_robot_in_a_box import get_sensor_types
 
 class BasicTests(unittest.TestCase):
     EXPECTED_SENSORS = {"/dev/ttyACM0": "ServoControl",
                         "/dev/ttyACM2": "YogiBear",
-                        "/dev/ttyACM1": "RFID"}
+                        "/dev/ttyACM1": "RFID",
+                        "/dev/ttyACM3": "LimitSwitch"}
     def setUpClass(self):
         from ..run_robot_in_a_box import stop_runtime
         stop_runtime()
@@ -34,5 +36,11 @@ class BasicTests(unittest.TestCase):
         for uid in sensors.values():
             # Shut up sensors
             process.subscribe(uid, 0, [])
-        # TODO: do actual testing
-        
+        for uid in sensors.values():
+            params = all_params_for_device_id(uid)
+            process.subscribe(uid, 1, params)
+        while True:
+            print(process.state_queue.get())
+
+if __name__ == "__main__":
+    unittest.main()
