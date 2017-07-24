320ef0f8f9ae59d79bc09d005291110c951690d0
baby-bell <baby-bell@users.noreply.github.com>
2017-07-23 18:06:36 -0700

    Update subscribe test to use timeout

--- hibike/riab_tests/basic.py
+++ hibike/riab_tests/basic.py
@@ -1,17 +1,20 @@
 import unittest
+import time
 from hibike_tester import Hibike
 from hibike_process import identify_smart_sensors, get_working_serial_ports
 import hibike_message as hm
-from run_robot_in_a_box import get_sensor_types
+from run_robot_in_a_box import get_sensor_types, get_sensor_uids
 
 class BasicTests(unittest.TestCase):
     EXPECTED_SENSORS = {"ServoControl", "YogiBear", "RFID", "LimitSwitch"}
+    # Check for all devices' UIDs until this many seconds.
+    DEVICE_READ_TIMEOUT = 5
     @classmethod
     def setUpClass(cls):
         from run_robot_in_a_box import stop_runtime
         stop_runtime()
 
-    @classmethod 
+    @classmethod
     def tearDownClass(cls):
         from run_robot_in_a_box import start_runtime
         start_runtime()
@@ -22,12 +25,14 @@ class BasicTests(unittest.TestCase):
         """
         sensors = set(get_sensor_types().values())
         self.assertEqual(self.EXPECTED_SENSORS, sensors)
-    
+
     def test_subscribe(self):
         """
         Subscribe to some devices and see what happens.
         """
-        sensors = get_sensor_types()
+        sensors = get_sensor_uids()
+        uids = set(sensors.values())
+        reads = {uid: False for uid in uids}
 
         process = Hibike()
         for uid in sensors.values():
@@ -38,11 +43,19 @@ class BasicTests(unittest.TestCase):
             params = hm.all_params_for_device_id(device_id)
             process.subscribe(uid, 1, params)
         # We should be receiving data
-        while True:
+        curr_time = time.time()
+        loop_time = time.time()
+        while loop_time - curr_time <= self.DEVICE_READ_TIMEOUT:
             packet = process.state_queue.get()
             if packet[0] == "device_values":
                 for uid in sensors.values():
-                    self.assertIn(uid, packet[1][0])
+                    reads[uid] = True
+            loop_time = time.time()
+
+        for (uid, read) in reads.items():
+            self.assertTrue(read,
+                            "subbed but didn't receive packet from {}".format(uid))
+
 
 if __name__ == "__main__":
     unittest.main()
