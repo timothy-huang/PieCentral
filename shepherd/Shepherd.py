6d31af9e4210bd489250a0d41fbb642f1b849d65
DAVID XIONG <DAVID.W.XIONG@GMAIL.COM>
2017-09-23 14:58:20 -0700

    [SHEPHERD] started work on field control events

--- shepherd/Shepherd.py
+++ shepherd/Shepherd.py
@@ -1,4 +1,3 @@
-"""
 import sys
 import Goal
 
@@ -12,29 +11,110 @@ class Shepherd:
 
         #The following are elements that are expected to exist in every iteration of the game
         self.UI = UI()
-        self.schedule = Schedule()
-        self.matchTimer = Timer()
+        self.schedule = Web()
+        self.match_timer = Timer()
+        self.match_number = 1
         teams = self.schedule.getTeams(matchNumber)
         self.alliances= {'blue': Alliance(teams[0], teams[1]), 'gold': Alliance(teams[2], teams[3])}
-        self.currentStage = 0
+        self.current_stage = 0
 
         #The follwing are elements that are configured
         specifically for the 2018 year game - Solar Scramble
         self.goals = {'a': Goal(),'b': Goal(),'c': Goal(),'d': Goal(),'e': Goal()}
 
-        self.goalTimers = {'a': Timer(),'b': Timer(),'c': Timer(),'d': Timer(),'e': Timer()}
-        self.decodeTimers = {'steal': Timer(), 'double': Timer(), 'zero': Timer()}
-        self.bidTimers = {'a': Timer(),'b': Timer(),'c': Timer(),'d': Timer(),'e': Timer()}
+        self.blue_powerup_timers = {'a': PowerupTimer(),
+                                    'b': PowerupTimer(),
+                                    'c': PowerupTimer(),
+                                    'd': PowerupTimer(),
+                                    'e': PowerupTimer()}
+        self.gold_powerup_timers = {'a': PowerupTimer(),
+                                    'b': PowerupTimer(),
+                                    'c': PowerupTimer(),
+                                    'd': PowerupTimer(),
+                                    'e': PowerupTimer()}
+        self.blue_decode_timers = PowerupTimer()
+        self.gold_decode_timers = PowerupTimer()
         self.sensors = Sensors()
+
+        self.event_mapping = {
+            "setup_match": setup_match,
+            "start_auto": start_auto,
+            "start_telop": start_telop,
+            "start_next_stage": start_next_stage,
+            "reset_match": reset_match,
+            "reset_stage": reset_stage,
+            "reset_lcm": reset_lcm,
+            "end_match": end_match,
+        }
+
+    def setup_match(self, matchNumber):
+        #Assuming the UI/ctrl station will provide us with a match number arg
+        self.match_number = matchNumber
+        teams = self.schedule.getTeams(self.match_number)
+        self.alliances= {'blue': Alliance(teams[0], teams[1]), 'gold': Alliance(teams[2], teams[3])}
+        self.current_stage = 'NONE'
+        self.timer = Timer()
+        broadcast_status("Set up match #" + str(self.match_number))
+
+    def start_auto(self):
+        self.current_stage = 'AUTO'
+        self.match_timer.reset()
+        self.set_driver_stations_mode("AUTO")
+        # assuming autonomous means we operate independently of the driver station
+        broadcast_status("Starting autonomous")
+        self.match_timer.start("AUTO_TIME")
+
+    def start_telop(self):
+        self.current_stage = 'TELOP'
+        self.match_timer.reset()
+        self.set_driver_stations_mode("TELOP")
+        broadcast_status("Starting teleop")
+        self.match_timer.start("TELOP_TIME")
+
+    def start_next_stage(self):
+        if self.current_stage == 'NONE':
+            self.start_auto()
+        else if self.current_stage == 'AUTO':
+            self.start_telop()
+        else:
+            self.end_match()
+
+    def end_match(self):
+        self.current_stage = 'NONE'
+        self.set_driver_stations_mode("NONE")
+        broadcast_status("Ending match")
+        # what else do we do?
+
+    def reset_match(self):
+        self.end_match()
+        self.setup_match()
+
+    def reset_stage(self):
+        # probably need to reset score to prev state as well (rollback), something for the alliance to handle?
+        # where do we even start
+
+    def reset_lcm(self):
+        # what happens here? can't we just force the lcm to do this itself?
+        
+
+    def set_driver_stations_mode(self, mode):
+        if mode == "AUTO":
+            
+        else if mode == "TELOP":
+            
+        else:
+
 
     Things to receive:
         UI_Commands from field control:
-            Start_Match
+            Setup_Match
             Start_Auto
             Start_Telop
+            Start_Next_Stage #added
             Reset_Match
             Reset_Stage
             reset_lcm (optional)
+            End_Match #added
         Button_Commands from bidding station:
             Bid on [Goal X] from [Alliance A]
         FromSensors:
@@ -48,14 +128,19 @@ class Shepherd:
         FromDriverStation:
             Robot State (connected, disconnected, teleop, auto)
 
+class PowerupTimer:
+    def __init__(self):
+        self.steal = Timer()
+        self.double = Timer()    
+        self.zero = Timer()
+
 def main():
-    shepherd = Shepherd(sys.argv[0])
-    shepherd.waitGameStart()
-    shepherd.autoLoop()
-    shepherd.teleopLoop()
-    shepherd.gameEnd()
-    shepherd.exportScore(sys.argv[0])
+    shepherd = Shepherd()
+    while True:
+
+
+
 
 if __name__ == '__main__':
     main()
-"""
+
