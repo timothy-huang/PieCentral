7a0515e74892c036c2f19fd5b9b716f5961f2072
Benji410 <benji410@berkeley.edu>
2017-10-14 15:05:50 -0700

    small changes to sendlogs

--- DevOps/frankfurter/scripts/tools/sendlogs.sh
+++ DevOps/frankfurter/scripts/tools/sendlogs.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+#make sure to install zip command using sudo apt install.. 
+
 mkdir temp && cd temp
 
 journalctl -e -u update.service > update.log
 journalctl -e -u runtime.service > service.log
-journalctl -e -u network.service > network.log
+journalctl -e -u networking.service > networking.log
+
+cd $HOME 
+
+
 
 
