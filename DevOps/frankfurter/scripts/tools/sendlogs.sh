93a2a62034c80b363810a3a197c8730147a62579
Benji410 <benji410@berkeley.edu>
2017-10-14 16:22:33 -0700

    added zip command

--- DevOps/frankfurter/scripts/tools/sendlogs.sh
+++ DevOps/frankfurter/scripts/tools/sendlogs.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 #make sure to install zip command using sudo apt install.. 
+#added zip pkg to master_frankfurter setup
 
 mkdir temp && cd temp
 
+#get the logs from these services and log them in temp directory
 journalctl -e -u update.service > update.log
 journalctl -e -u runtime.service > service.log
 journalctl -e -u networking.service > networking.log
 
 cd $HOME 
+
+#zip log files and print out zip file if it was made
+zip -r service-logs.zip temp && ls *.zip
+
+#send files to host computer
+
 
 
 
